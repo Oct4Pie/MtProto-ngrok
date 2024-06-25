94dad72bf18999dd4ca944e1f0b487d5400182f1
Oct4pie <>
2024-06-24 23:13:46 -0700

    formatting

--- config.py
+++ config.py
@@ -7,17 +7,14 @@ PORT = 4443
 USERS = {
     "tg": open("secret", "r").read() if os.path.exists("secret") else "",
 }
-
 MODES = {
     "classic": False,
     "secure": True,
     "tls": True,
 }
-
 TLS_DOMAIN = random.choice(
     [
         "www.ngrok.com",
-
     ]
 )
 AUTHTOKEN = ""  # set to your ngrok's token
